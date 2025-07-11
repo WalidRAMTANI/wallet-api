f67748c3d70c84dbdf3c905b81733893165839c0
WalidRAMTANI <walid.ramtani@yahoo.com>
2025-07-11 16:04:18 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,14 +4,19 @@ import dotenv from 'dotenv';
 import transactionRoute from './routes/transactions.route.js';
 import ratelimit from './middleware/rateLimiter.js'; // Import the rate limiting middleware
 import { connectToDatabase } from './config/db.js';
+import job from './config/cron.js';
 dotenv.config();
 
 const app = expess();
+
+if(process.env.NODE_ENV === "production") job.start(); // Start the cron job to send GET requests every 14 minutes
 const PORT = process.env.PORT || 5001;
 app.use(cors());
 app.use(expess.json());
 app.use(ratelimit); // Apply rate limiting middleware
-
+app.get('/api/health', (req, res) => {
+  res.send('Welcome to the Transactions API');
+});
 app.use('/api/transactions', transactionRoute);
 
 // API endpoints
